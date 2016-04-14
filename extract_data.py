d70cb15f4119fce8e6e6118471363331cb81adf1
michal_januszkiewicz <mljc92@gmail.com>
2016-04-14 10:45:48 +0200

    gzip the extracted data output file

--- extract_data.py
+++ extract_data.py
@@ -7,11 +7,11 @@ output_path = sys.argv[2]
 quantity		= int(sys.argv[3])
 
 i = 0
-f = open(output_path, 'w')
+f = gzip.open(output_path + '.gz', 'w')
 g = gzip.open(input_path, 'r')
 for l in g:
 	line = json.dumps(eval(l))
-	f.write(line + '\n')
+	f.write(bytes(line + '\n', 'UTF-8'))
 	i += 1
 	if i == quantity:
 		f.close()
