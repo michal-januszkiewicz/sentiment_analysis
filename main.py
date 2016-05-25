f1bb545aec6f59f71af59f261b29da1ba1944d43
michal <mljc92@gmail.com>
2016-05-25 17:19:48 +0200

    Calculate sentiment analysis for different parameters

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from sentiment_analysis import *
 
-calculate(100, 10, 10, 5000, True)
+n_estimators_array            = [100, 1000]
+max_depth_array               = [100, 1000]
+max_features_array            = [100, 1000]
+vectorizer_max_features_array = [5000, 10000]
+neutral_sentiment_array       = [True, False]
+
+for n_estimators in n_estimators_array:
+  for max_depth in max_depth_array:
+    for max_features in max_features_array:
+      for vectorizer_max_features in vectorizer_max_features_array:
+        for neutral_sentiment in neutral_sentiment_array:
+          calculate(n_estimators, max_depth, max_features, 
+              vectorizer_max_features, neutral_sentiment)
+
+
+
